eba4cff22b20f972e2bb410c8bf53febfad4b700
jstaffans <johannes.staffans@gmail.com>
2012-03-05 00:36:28 +0200

    map search, host information page, icons

--- src/fi/bitrite/android/ws/model/Host.java
+++ src/fi/bitrite/android/ws/model/Host.java
@@ -54,19 +54,27 @@ public class Host extends _Host {
 		return sb.append(getPostalCode()).append(", ").append(getCity()).append(", ").append(getProvince())
 				.append(", ").append(getCountry().toUpperCase()).toString();
 	}
-	
+
 	public String getServices() {
 		StringBuilder sb = new StringBuilder();
-		
-		if (getShower().equals("1")) 		sb.append("Shower\n");
-		if (getFood().equals("1")) 			sb.append("Food\n");
-		if (getBed().equals("1")) 			sb.append("Bed\n"); 
-		if (getLaundry().equals("1")) 		sb.append("Laundry\n");
-		if (getStorage().equals("1")) 		sb.append("Storage\n");
-		if (getKitchenUse().equals("1"))	sb.append("Use of kitchen\n");
-		if (getLawnspace().equals("1"))		sb.append("Lawn space (for camping)\n");
-		if (getSag().equals("1"))			sb.append("SAG (vehicle support)\n");
-	
+
+		if (getShower().equals("1"))
+			sb.append("Shower\n");
+		if (getFood().equals("1"))
+			sb.append("Food\n");
+		if (getBed().equals("1"))
+			sb.append("Bed\n");
+		if (getLaundry().equals("1"))
+			sb.append("Laundry\n");
+		if (getStorage().equals("1"))
+			sb.append("Storage\n");
+		if (getKitchenUse().equals("1"))
+			sb.append("Use of kitchen\n");
+		if (getLawnspace().equals("1"))
+			sb.append("Lawn space (for camping)\n");
+		if (getSag().equals("1"))
+			sb.append("SAG (vehicle support)\n");
+
 		return sb.toString();
 	}
 
