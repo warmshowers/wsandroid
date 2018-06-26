e16b330c0bb9a734ded0b122b7dd62b9df0fa35b
Sämy Zehnder <saemy.zehnder@gorrion.ch>
2018-06-26 19:00:22 +0200

    Add more lifecycle disposables to BaseFragment
    Adds CompositeDisposables for the create/destroy view and start/stop
    lifecycle events.

--- app/src/main/java/fi/bitrite/android/ws/ui/BaseFragment.java
+++ app/src/main/java/fi/bitrite/android/ws/ui/BaseFragment.java
@@ -1,7 +1,12 @@
 package fi.bitrite.android.ws.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import javax.inject.Inject;
 
@@ -15,6 +20,8 @@ public abstract class BaseFragment extends Fragment implements Injectable {
     @Inject ActionBarTitleHelper mActionBarTitleHelper;
 
     private CompositeDisposable mCreateDestroyDisposable = new CompositeDisposable();
+    private CompositeDisposable mCreateDestroyViewDisposable = new CompositeDisposable();
+    private CompositeDisposable mStartStopDisposable = new CompositeDisposable();
     private CompositeDisposable mResumePauseDisposable = new CompositeDisposable();
 
     @Override
@@ -22,17 +29,38 @@ public abstract class BaseFragment extends Fragment implements Injectable {
         super.onCreate(savedInstanceState);
         mCreateDestroyDisposable = new CompositeDisposable();
     }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mCreateDestroyViewDisposable = new CompositeDisposable();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onStart() {
+        mStartStopDisposable = new CompositeDisposable();
+        super.onStart();
+    }
     @Override
     public void onResume() {
-        super.onResume();
-
         mResumePauseDisposable = new CompositeDisposable();
+        super.onResume();
         setTitle(getTitle());
     }
     @Override
     public void onPause() {
         mResumePauseDisposable.dispose();
         super.onPause();
+    }
+    @Override
+    public void onStop() {
+        mStartStopDisposable.dispose();
+        super.onStop();
+    }
+    @Override
+    public void onDestroyView() {
+        mCreateDestroyViewDisposable.dispose();
+        super.onDestroyView();
     }
     @Override
     public void onDestroy() {
@@ -51,6 +79,12 @@ public abstract class BaseFragment extends Fragment implements Injectable {
 
     CompositeDisposable getCreateDestroyDisposable() {
         return mCreateDestroyDisposable;
+    }
+    CompositeDisposable getCreateDestroyViewDisposable() {
+        return mCreateDestroyViewDisposable;
+    }
+    CompositeDisposable getStartStopDisposable() {
+        return mStartStopDisposable;
     }
     CompositeDisposable getResumePauseDisposable() {
         return mResumePauseDisposable;
