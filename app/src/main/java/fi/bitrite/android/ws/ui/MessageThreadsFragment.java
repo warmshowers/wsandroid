fc7d0f7437adb1394e8366b503812fbae5249f27
Mariha Kozinska <m.kozinska@gmail.com>
2020-04-08 23:23:49 -0500

    Exception handling for message threads

--- app/src/main/java/fi/bitrite/android/ws/ui/MessageThreadsFragment.java
+++ app/src/main/java/fi/bitrite/android/ws/ui/MessageThreadsFragment.java
@@ -6,7 +6,6 @@ import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
