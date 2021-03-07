c464646544102bdbd45af10d53e77aba319aaba8
Mariha Kozinska <m.kozinska@gmail.com>
2021-03-07 06:28:06 +0100

    Message in html so that we can highlight most important parts.

--- app/src/main/java/fi/bitrite/android/ws/ui/ActivityMovingOut.java
+++ app/src/main/java/fi/bitrite/android/ws/ui/ActivityMovingOut.java
@@ -1,6 +1,9 @@
 package fi.bitrite.android.ws.ui;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
@@ -8,27 +11,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import fi.bitrite.android.ws.R;
 
 /**
- * The activity responsible for getting Warmshowers credentials from the user,
- * verifying them against the Warmshowers web service and storing
- * them on the device using Android's custom account facilities.
+ * The activity displays Goodbye Warmshowers message to the user.
  */
 public class ActivityMovingOut extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_moving_out);
-        TextView tv = findViewById(R.id.moving_out_title_description);
-        tv.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView message = findViewById(R.id.moving_out_message);
+        message.setText(getFormattedText(getString(R.string.moving_out_message)));
+        message.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
+    private Spanned getFormattedText(String text) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ?
+                Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT) :
+                Html.fromHtml(text);
     }
 }
